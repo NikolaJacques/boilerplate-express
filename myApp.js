2e1cedd6a9b1bdd464c8bbd0255df764201b15db
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-17 19:07:36 +0100

    test

--- myApp.js
+++ myApp.js
@@ -7,22 +7,15 @@ app.get('/', (req,res) => {
     res.send('Hello Express');
 }); */
 
-<<<<<<< HEAD
 const staticPath = __dirname + '/public';
 
 app.use(express.static(staticPath));
 
-=======
->>>>>>> parent of 74685e9 (changed code order)
 const absolutePath = __dirname + '/views/index.html';
 
 app.get('/', (req,res) => {
     res.sendFile(absolutePath);
 });
-
-const staticPath = __dirname + '/public';
-
-app.use(express.static(staticPath));
 
 
 
