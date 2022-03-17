68fe6520843353d7905b8820fed149c64d4cbea3
NikolaJacques <44374495+NikolaJacques@users.noreply.github.com>
2022-03-17 18:36:56 +0100

    added route for sending file

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
-console.log('Hello World');
+/* console.log('Hello World');
 
 app.get('/', (req,res) => {
     res.send('Hello Express');
-});
+}); */
 
+const absolutePath = __dirname + '/views/index.html';
+
+app.get('/', (req,res) => {
+    res.sendFile(absolutePath);
+});
 
 
 
